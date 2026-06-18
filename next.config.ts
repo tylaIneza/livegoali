0a712ae7cfb39228ef011fbf95e0b067523323b9
Tyla Paccy <tylapaccy@192.168.1.67>
2026-06-19 00:49:41 +0200

    Allow all image domains for league/team logos

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "res.cloudinary.com" },
-      { protocol: "https", hostname: "lh3.googleusercontent.com" },
-      { protocol: "https", hostname: "upload.wikimedia.org" },
-      { protocol: "https", hostname: "**.imgur.com" },
-      { protocol: "https", hostname: "crests.football-data.org" },
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" },
     ],
   },
   serverExternalPackages: ["bcryptjs", "pg"],
